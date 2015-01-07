62a437136bd8b3373347f2da342b84174f5d32a8
Pat Cavit <pcavit@gmail.com>
2015-01-06 16:41:52 -0800

    Authentication via OAuth2
    This needs to be cleaned up later, obviously.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ server.route({
     path : "/",
     handler : function(req, reply) {
         reply("hi");
+    },
+    config : {
+        auth : false
     }
 });
 
@@ -33,11 +36,67 @@ server.register([
         options  : {
             reporters : [{
                 reporter : require("good-console"),
-                args : [{ log : "*", request : "*" }]
+                args     : [{ log : "*", ops : "*", response : "*", error : "*" }]
             }]
         }
     },
 ], throwError);
+
+// Cookie auth on most routes
+server.register(require("hapi-auth-cookie"), function(err) {
+    if(err) {
+        throw err;
+    }
+
+    server.auth.strategy("session", "cookie", "required", {
+        // TODO: This needs to be a real password
+        password : "secret",
+        cookie   : "s",
+        isSecure : false
+    });
+});
+
+// OAuth, has to come before any of our routes are added
+server.register(require("bell"), function(err) {
+    if(err) {
+        throw err;
+    }
+
+    server.auth.strategy("oauth", "bell", {
+        provider : {
+            protocol : "oauth2",
+            auth     : "https://account-dev.ncplatform.net/oauth2/authorization",
+            token    : "https://account-dev.ncplatform.net/oauth2/token",
+            version  : "2",
+            scope    : [ "account" ]
+        },
+        // TODO: This needs to be a real password
+        password : "fooga",
+        clientId : "43B254C9-F61B-E311-AEF8-0017A47704A4",
+        clientSecret : "DA15D9B7-60EE-47E2-9CCF-525F20FE7EB0",
+        isSecure : false
+    });
+
+    server.route({
+        method : [ "GET", "POST" ],
+        path : "/login",
+        config : {
+            auth : {
+                strategy : "oauth",
+                mode : "try"
+            },
+            handler : function(req, reply) {
+                if(!req.auth.isAuthenticated) {
+                    return reply("Authentication failed due to: " + req.auth.error.message);
+                }
+
+                req.auth.session.set(req.auth.credentials);
+                
+                reply("authed");
+            }
+        }
+    });
+});
 
 // Simpleton plugins
 server.register([
