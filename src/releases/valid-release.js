c036f467321bf76b41b31849b50fabc2e552a427
Pat Cavit <pcavit@gmail.com>
2015-01-06 17:12:09 -0800

    Experimenting with more joi data for better docs

--- src/releases/valid-release.js
+++ src/releases/valid-release.js
@@ -3,6 +3,10 @@
 var joi  = require("joi");
 
 module.exports = {
-    name : joi.string().min(1),
-    live : joi.date().optional()
+    name : joi.string().min(1)
+        .description("User-friendly name for this release")
+        .example("WintersDay 2025"),
+    live : joi.date()
+        .optional()
+        .description("The date this release goes live")
 };
--- src/types/valid-type.js
+++ src/types/valid-type.js
@@ -11,11 +11,18 @@ var joi = require("joi"),
     ];
 
 module.exports = {
-    name   : joi.string().required(),
+    name   : joi.string().min(1)
+        .required()
+        .description("User-friendly name for this content type")
+        .example("Influencer Post"),
     fields : joi.array().includes(
         {
-            name : joi.string().min(1).required(),
-            type : joi.string().valid(fields).required(),
+            name : joi.string().min(1)
+                .required()
+                .description("User-friendly name for this field")
+                .example("Title"),
+            type : joi.string().valid(fields)
+                .required(),
             required : joi.boolean()
         }
     )
